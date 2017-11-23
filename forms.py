f7c79490735da781515a9c6342710fb2df6183f2
nazomeku <cols.nazo@gmail.com>
2017-11-23 16:35:17 +0100

    add login

--- forms.py
+++ forms.py
@@ -8,3 +8,8 @@ class SignupForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired("Please enter your email address."), Email("Please enter your email address.")])
     password = PasswordField('Password', validators=[DataRequired("Please enter your password."), Length(min=6, message="Passwords must be at least 6 characters long.")])
     submit = SubmitField('Sign up')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired("Please enter your email address."), Email("Please enter your email address.")])
+    password = PasswordField('Password', validators=[DataRequired("Please enter your password.")])
+    submit = SubmitField("Log in")
--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
 from models import db, User
-from forms import SignupForm
+from forms import SignupForm, LoginForm
 
 app = Flask(__name__)
 
@@ -9,13 +9,13 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 app.secret_key = "devkey"
 
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
-@app.route("/about")
+@app.route('/about')
 def about():
-    return render_template("about.html")
+    return render_template('about.html')
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -26,9 +26,35 @@ def signup():
         newuser = User(form.first_name.data, form.last_name.data, form.email.data, form.password.data)
         db.session.add(newuser)
         db.session.commit()
-        return 'Success!'
+        session['email'] = newuser.email
+        return redirect(url_for('home'))
     elif request.method == 'GET':
         return render_template('signup.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if request.method == 'POST':
+        if form.validate() is False:
+            return render_template('login.html', form=form)
+        email = form.email.data
+        password = form.password.data
+        user = User.query.filter_by(email=email).first()
+        if user is not None and user.check_password(password):
+            session['email'] = form.email.data
+            return redirect(url_for('home'))
+        return redirect(url_for('login'))
+    elif request.method == 'GET':
+        return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout():
+    session.pop('email', None)
+    return redirect(url_for('index'))
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
